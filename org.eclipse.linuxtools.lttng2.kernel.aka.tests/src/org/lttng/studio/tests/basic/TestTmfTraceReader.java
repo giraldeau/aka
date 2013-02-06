772e8d08af6e3650f8e799c06b97032c9d38617c
Francis Giraldeau <francis.giraldeau@gmail.com>
2013-02-06 11:47:04 -0500

    Add test for TMF requests

---
+++ org.eclipse.linuxtools.lttng2.kernel.aka.tests/src/org/lttng/studio/tests/basic/TestTmfTraceReader.java
@@ -0,0 +1,54 @@
+package org.lttng.studio.tests.basic;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.linuxtools.tmf.core.ctfadaptor.CtfTmfEvent;
+import org.eclipse.linuxtools.tmf.core.ctfadaptor.CtfTmfTrace;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
+import org.eclipse.linuxtools.tmf.core.request.TmfDataRequest;
+import org.junit.Test;
+
+/*
+ * Test TmfProvider requests
+ */
+public class TestTmfTraceReader {
+
+	int count;
+	TmfDataRequest request;
+
+	@Test
+	public void testReadFromTmfProvider() throws TmfTraceException, IOException, InterruptedException {
+		count = 0;
+		File trace = TestTraceset.getKernelTrace("sleep-1x-1sec-k");
+		CtfTmfTrace ctfTrace = new CtfTmfTrace();
+		ctfTrace.initTrace(null, trace.getCanonicalPath(), CtfTmfEvent.class);
+		request = new TmfDataRequest(ITmfEvent.class) {
+			@Override
+			public void handleData(final ITmfEvent event) {
+				if (event instanceof CtfTmfEvent) {
+					count++;
+					request.cancel();
+				}
+			}
+			@Override
+			public void handleCancel() {
+
+			}
+			@Override
+			public void handleSuccess() {
+
+			}
+			@Override
+			public void handleFailure() {
+
+			}
+		};
+		ctfTrace.sendRequest(request);
+		request.waitForCompletion();
+		assertEquals(1, count);
+	}
+}
