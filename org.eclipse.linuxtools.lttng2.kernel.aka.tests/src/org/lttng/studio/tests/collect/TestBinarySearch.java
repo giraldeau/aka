e5b1f269dd35dffafaefd610af393ddc1c07ae78
Francis Giraldeau <francis.giraldeau@gmail.com>
2013-02-13 14:31:17 -0500

    Range selection based on binary search
    Select range of items in a list. Indexes of the sub list range are computed
    with a binary search.

--- org.eclipse.linuxtools.lttng2.kernel.aka.tests/src/org/lttng/studio/tests/collect/TestBinarySearch.java
+++ org.eclipse.linuxtools.lttng2.kernel.aka.tests/src/org/lttng/studio/tests/collect/TestBinarySearch.java
@@ -5,19 +5,25 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.lttng.studio.collect.BinarySearch;
 
 public class TestBinarySearch {
 
-	@Test
-	public void testBinarySearch1() {
-		List<Integer> list = new ArrayList<Integer>();
+	private List<Integer> list;
+
+	@Before
+	public void setup() {
+		list = new ArrayList<Integer>();
 		// [100, 200, 300, 400, 500, 600, 700, 800, 900] size = 9
 		for (int i = 100; i < 1000; i += 100) {
 			list.add(i);
 		}
+	}
 
+	@Test
+	public void testBinarySearch1() {
 		int[][] floor = new int[][] { {Integer.MIN_VALUE, -1}, {0, -1},
 				{99, -1}, {100, 0}, {101, 0}, {899, 7}, {900, 8},
 				{901, 8}, {Integer.MAX_VALUE, 8} };
@@ -29,6 +35,24 @@ public class TestBinarySearch {
 			assertEquals(ceiling[i][1], BinarySearch.ceiling(list, ceiling[i][0]));
 			//System.out.println("exp = " + floor[i][1] + " floor(" + floor[i][0] + ") = " + BinarySearch.floor(list, floor[i][0]));
 			//System.out.println("exp = " + ceiling[i][1] + " ceiling(" + ceiling[i][0] + ") = " + BinarySearch.ceiling(list, ceiling[i][0]));
+		}
+	}
+
+	@Test
+	public void testBinarySearch2() {
+		int[][] ranges = new int[][] {
+				{Integer.MIN_VALUE, Integer.MIN_VALUE + 1, 0},
+				{-1,	1,		0},
+				{0, 	99, 	0},
+				{0, 	100,	0},
+				{100, 	101,	1},
+				{0, 	900,	8},
+				{0, 	901,	9},
+				{801, 	1000,	2},
+		};
+		for (int i = 0; i < ranges.length; i++) {
+			assertEquals(ranges[i][2], BinarySearch.range(list, ranges[i][0], ranges[i][1]).size());
+			//System.out.println("" + BinarySearch.range(list, ranges[i][0], ranges[i][1]));
 		}
 	}
 
