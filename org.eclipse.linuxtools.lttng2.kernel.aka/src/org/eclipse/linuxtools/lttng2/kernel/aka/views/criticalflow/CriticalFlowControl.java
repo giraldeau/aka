9c20d48dc6db42f44ef54133e17c0f16851d008f
Francis Giraldeau <francis.giraldeau@gmail.com>
2013-04-05 14:04:35 -0400

    Updated version of drawArrow
    This version uses pre computed trigonometric values. Draws the arrow at the end
    of the line.

--- org.eclipse.linuxtools.lttng2.kernel.aka/src/org/eclipse/linuxtools/lttng2/kernel/aka/views/criticalflow/CriticalFlowControl.java
+++ org.eclipse.linuxtools.lttng2.kernel.aka/src/org/eclipse/linuxtools/lttng2/kernel/aka/views/criticalflow/CriticalFlowControl.java
@@ -22,7 +22,7 @@ public class CriticalFlowControl extends TimeGraphControl {
 	@Override
 	public void drawItems(Rectangle bounds, ITimeDataProvider timeProvider,
 			Item[] items, int topIndex, int nameSpace, GC gc) {
-		super.drawItems(bounds, timeProvider, items, topIndex, nameSpace, gc);
+;		super.drawItems(bounds, timeProvider, items, topIndex, nameSpace, gc);
 		for (int i = topIndex; i < items.length; i++) {
 			Item item = items[i];
 			drawLinks(item, bounds, timeProvider, i, nameSpace, gc);
@@ -56,8 +56,7 @@ public class CriticalFlowControl extends TimeGraphControl {
 					int y0 = rect.y + offset;
 					int y1 = dst.y + offset;
 					gc.drawLine(x0, y0, x1, y1);
-					drawArrow(x1, x0, y1, y0, gc);
-					//drawArrow2(x0, y0, x1, y1, gc);
+					drawArrow(x0, y0, x1, y1, gc);
 				}
 			}
 		}
@@ -68,44 +67,23 @@ public class CriticalFlowControl extends TimeGraphControl {
 	 * Source:
 	 * http://stackoverflow.com/questions/3010803/draw-arrow-on-line-algorithm
 	 */
-	public static void drawArrow(int tipX, int tailX, int tipY, int tailY, GC gc) {
-		int arrowLength = 12;
-		int angle = 20;
-		int dx = tipX - tailX;
-		int dy = tipY - tailY;
-
-		int posx = tailX + dx / 2;
-		int posy = tailY + dy / 2;
-
-		double theta = Math.atan2(dy, dx);
-
-		double rad = Math.toRadians(angle);
-		long x = Math.round(posx - arrowLength * Math.cos(theta + rad));
-		long y = Math.round(posy - arrowLength * Math.sin(theta + rad));
-
-		double phi2 = Math.toRadians(-angle);
-		long x2 = Math.round(posx - arrowLength * Math.cos(theta + phi2));
-		long y2 = Math.round(posy - arrowLength * Math.sin(theta + phi2));
-
-		int[] arrow = new int[] { posx, posy, (int) x, (int) y, (int) x2,
-				(int) y2 };
-		gc.fillPolygon(arrow);
-	}
-
-	private static void drawArrow2(int x0, int y0, int x1, int y1, GC gc)
+	private static void drawArrow(int x0, int y0, int x1, int y1, GC gc)
 	{
-		double cos = 0.866;
-		double sin = 0.500;
-		float len = 1;//Math.sqrt(((double)(x1 - x0)^2 + (y1 - y0)^2));
+		int factor = 10; 
+		double cos = 0.9510;
+		double sin = 0.3090;
+		int lenx = x1 - x0;
+		int leny = y1 - y0;
+		double len = Math.sqrt((double) (lenx * lenx  + leny * leny));
 		
-		float dx = (x1 - x0) / len;
-		float dy = (y1 - y0) / len;
-		int end1X = (int)(x1 + (dx * cos + dy * -sin));
-		int end1Y = (int)(y1 + (dx * sin + dy * cos));
-		int end2X = (int)(x1 + (dx * cos + dy * sin));
-		int end2Y = (int)(y1 + (dx * -sin + dy * cos));
-		gc.drawLine(x1, y1, end1X, end1Y);
-		gc.drawLine(x1, y1, end2X, end2Y);
+		double dx = factor * lenx / len;
+		double dy = factor * leny / len;
+		int end1X = (int) Math.round((x1 + (dx * cos + dy * -sin)));
+		int end1Y = (int) Math.round((y1 - (dx * sin + dy * cos)));
+		int end2X = (int) Math.round((x1 + (dx * cos + dy * sin)));
+		int end2Y = (int) Math.round((y1 - (dx * -sin + dy * cos)));
+		int[] arrow = new int[] { x1, y1, end1X, end1Y, end2X, end2Y, x1, y1 };
+		gc.fillPolygon(arrow);
 	}
 	
 }
